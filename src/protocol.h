3275fec15c0315bb3829f0297078734ac4c56fe4
mountaind <trumpinsane1@mail.com>
2016-12-06 00:58:18 +0100

    p2p testnet

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 33542 : 8028; //
+    return testnet ? 18028 : 8028; //
 }
 
 
